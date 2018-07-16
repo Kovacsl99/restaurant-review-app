271bab168ed2e93d23709bfc1f1a9fa1413eebdf
Lajos <34096598+Kovacsl99@users.noreply.github.com>
2018-07-16 11:30:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -49,7 +49,7 @@ self.addEventListener('activate', function(event) {
 });
 
 // Fetch events handling
-self.addEventListener('fetch', function(event) {
+/*self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
@@ -58,4 +58,42 @@ self.addEventListener('fetch', function(event) {
         console.log(err, event.request);
       })
     );
+});*/
+
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(actualCacheName)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
+
